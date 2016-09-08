6cc399deeb9d366e02c0a6b80020199734c09d52
Uku Tammet <uku.tammet@transferwise.com>
2016-09-08 23:06:08 +0300

    Update 2016-09-08T20:05:58.306Z

--- src/scripts/constants/errorConstants.js
+++
@@ -1,2 +0,0 @@
-export const SET_ERROR = 'SET_ERROR';
-export const REMOVE_ERROR = 'REMOVE_ERROR';
--- src/scripts/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import entities from './entitiesReducer';
-import error from './errorReducer';
-
-export default combineReducers({
-  entities,
-  error,
-});
